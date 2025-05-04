9e4d47a9c55660383273cdd0756c932ebef4b5b0
alexesp <alexizaes@hotmail.com>
2025-05-04 13:54:47 +0200

    Crear plantillas

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,7 @@ import (
 	"github.com/alexesp/FiberWebApp/config"
 	"github.com/alexesp/FiberWebApp/internal/home"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/gofiber/template/html/v2"
 )
 
 //func start(c *fiber.Ctx) error {
@@ -18,16 +16,17 @@ func main() {
 	config.Init()
 	config.NewDatabaseConfig()
 
-	logConfig := config.NewLogConfig()
+	//logConfig := config.NewLogConfig()
 	//dbConf := config.NewDatabaseConfig()
 	//log.Println(dbConf)
 
 	//log.SetLevel(log.LevelTrace)
-	log.SetLevel(log.Level(logConfig.Level))
-	engine := html.New("./html", ".html")
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
+	// log.SetLevel(log.Level(logConfig.Level))
+	// engine := html.New("./html", ".html")
+	// app := fiber.New(fiber.Config{
+	// 	Views: engine,
+	// })
+	app := fiber.New()
 	app.Use(recover.New())
 	//app.Get("/", start)
 	home.NewHandler(app)
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/alexesp/FiberWebApp
 go 1.23.5
 
 require (
+	github.com/a-h/templ v0.3.865 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/gofiber/fiber/v2 v2.52.6 // indirect
 	github.com/gofiber/template v1.8.3 // indirect
@@ -18,5 +19,5 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
 )
